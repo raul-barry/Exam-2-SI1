944e57c82c86c08b36334f83402ccf3932041000
cristiansantiagochoquecanavide <cristiansantiagochoquecanavide@gmail.com>
2025-11-13 23:45:43 -0400

    Update database.php configuration

--- config/database.php
+++ config/database.php
@@ -1,4 +1,4 @@
-<?php
+7<?php
 
 use Illuminate\Support\Str;
 
@@ -84,19 +84,20 @@ return [
         ],
 
         'pgsql' => [
-            'driver' => 'pgsql',
-            'url' => env('DB_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'laravel'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
-            'charset' => env('DB_CHARSET', 'utf8'),
-            'prefix' => '',
-            'prefix_indexes' => true,
-            'search_path' => env('DB_SCHEMA', 'carga_horaria'),
-            'sslmode' => env('DB_SSLMODE', 'prefer'),
-        ],
+    'driver' => 'pgsql',
+    'url' => env('DB_URL'),
+    'host' => null,
+    'port' => null,
+    'database' => null,
+    'username' => null,
+    'password' => null,
+    'charset' => 'utf8',
+    'prefix' => '',
+    'prefix_indexes' => true,
+    'search_path' => env('DB_SCHEMA', 'carga_horaria'),
+    'sslmode' => env('DB_SSLMODE', 'require'),
+],
+
 
         'sqlsrv' => [
             'driver' => 'sqlsrv',
