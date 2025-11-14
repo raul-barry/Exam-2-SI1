7b8c624b59d6d4ec089efcdfa3df2967867a1bcf
cristiansantiagochoquecanavide <cristiansantiagochoquecanavide@gmail.com>
2025-11-14 03:29:04 -0400

    Commit latest updates including manifest.json changes

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,17 @@
-import { defineConfig } from 'vite'
-import laravel from 'laravel-vite-plugin'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [
         laravel({
             input: [
+                'resources/css/app.css',
                 'resources/js/app.jsx',
-                'resources/css/app.css',  // ← Faltaba esto
-                'resources/css/CRUD.css',
-               'resources/css/sidebar.css'
             ],
             refresh: true,
         }),
         react(),
     ],
-})
+});
 
